42dd5a752a6d844152e7027809c07b2beee050a0
walterjrr <jrwalter731@gmail.com>
2022-05-27 17:14:04 -0300

    style complete

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,108 +1,118 @@
 <template>
   <div class="container">
-    <b-form @submit.stop.prevent="onSubmit" @reset="onReset" v-if="show">
-      <h1>Sobre o profissional</h1>
-      <h2>Dados do profissional</h2>
-      <b-form-group label="Nome Completo*" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="$v.form.name.$model"
-          placeholder="Digite o nome completo"
-          aria-describedby="input-1-live-feedback"
-          :state="validateState('name')"
-          style="{input}"
-        ></b-form-input>
+    <div class="formContainer">
+      <b-form @submit.stop.prevent="onSubmit" v-if="show">
+        <h1>Sobre o profissional</h1>
+        <h2>Dados do profissional</h2>
+        <b-form-group label="Nome Completo*" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="$v.form.name.$model"
+            placeholder="Digite o nome completo"
+            aria-describedby="input-1-live-feedback"
+            :state="validateState('name')"
+            style="{input}"
+          ></b-form-input>
 
-        <b-form-invalid-feedback id="input-1-live-feedback"
-          >This is a required field and must be at least 3
-          characters.</b-form-invalid-feedback
-        >
-      </b-form-group>
-
-      <b-form-group label="CPF*" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="$v.form.cpf.$model"
-          placeholder="Digite um CPF"
-          aria-describedby="input-1-live-feedback"
-          :state="validateState('cpf')"
-        ></b-form-input>
-
-        <b-form-invalid-feedback id="input-1-live-feedback"
-          >This is a required field and must be at least 3
-          characters.</b-form-invalid-feedback
-        >
-      </b-form-group>
-
-      <b-form-group label="Numero de Celular*" label-for="input-3">
-        <b-form-input
-          v-model="$v.form.number.$model"
-          placeholder="(00) 0 0000-0000"
-          aria-describedby="input-1-live-feedback"
-          :state="validateState('number')"
-        ></b-form-input>
-
-        <b-form-invalid-feedback id="input-1-live-feedback"
-          >This is a required field and must be at least 3
-          characters.</b-form-invalid-feedback
-        >
-      </b-form-group>
-
-      <div style="display: flex">
-        <b-form-group label="Estado*" label-for="example-input-2">
-          <b-form-select
-            id="example-input-2"
-            v-model="form.state"
-            name="example-input-2"
-            :options="state"
-            aria-describedby="input-2-live-feedback"
-          ></b-form-select>
-          <b-form-invalid-feedback id="input-2-live-feedback"
+          <b-form-invalid-feedback id="input-1-live-feedback"
             >This is a required field and must be at least 3
             characters.</b-form-invalid-feedback
           >
         </b-form-group>
 
-        <b-form-group label="Cidade*" label-for="example-input-2">
-          <b-form-select
-            id="example-input-2"
-            v-model="form.city"
-            name="example-input-2"
-            :options="city"
-            aria-describedby="input-2-live-feedback"
-          ></b-form-select>
-          <b-form-invalid-feedback id="input-2-live-feedback"
+        <b-form-group label="CPF*" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="$v.form.cpf.$model"
+            placeholder="Digite um CPF"
+            aria-describedby="input-1-live-feedback"
+            :state="validateState('cpf')"
+          ></b-form-input>
+
+          <b-form-invalid-feedback id="input-1-live-feedback"
             >This is a required field and must be at least 3
             characters.</b-form-invalid-feedback
           >
         </b-form-group>
-      </div>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
+        <b-form-group label="Numero de Celular*" label-for="input-3">
+          <b-form-input
+            v-model="$v.form.number.$model"
+            placeholder="(00) 0 0000-0000"
+            aria-describedby="input-1-live-feedback"
+            :state="validateState('number')"
+          ></b-form-input>
+
+          <b-form-invalid-feedback id="input-1-live-feedback"
+            >This is a required field and must be at least 3
+            characters.</b-form-invalid-feedback
+          >
+        </b-form-group>
+
+        <div class="Statecity">
+          <b-form-group label="Estado*" label-for="example-input-2">
+            <b-form-select
+              id="example-input-1"
+              v-model="form.state"
+              name="example-input-2"
+              :options="state"
+              aria-describedby="input-2-live-feedback"
+            ></b-form-select>
+            <b-form-invalid-feedback id="input-2-live-feedback"
+              >This is a required field and must be at least 3
+              characters.</b-form-invalid-feedback
+            >
+          </b-form-group>
+
+          <b-form-group label="Cidade*" label-for="example-input-2">
+            <b-form-select
+              id="example-input-2"
+              v-model="form.city"
+              name="example-input-2"
+              :options="city"
+              aria-describedby="input-2-live-feedback"
+            ></b-form-select>
+            <b-form-invalid-feedback id="input-2-live-feedback"
+              >This is a required field and must be at least 3
+              characters.</b-form-invalid-feedback
+            >
+          </b-form-group>
+        </div>
+
+        <button type="submit" variant="primary">PROXIMO</button>
+      </b-form>
+    </div>
+    <div class="img">
+      <img src="../assets/desktop-pagina-1.png" alt="imagem de dois medicos">
+    </div>
   </div>
 </template>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400&display=swap");
 
-
 * {
   box-sizing: border-box;
   font-family: "Confortaa";
+  padding-bottom: 8px;
 }
 body {
-  padding: 30px;
-  background: yellow;
+  padding: 100px;
+  background: #ffe766;
 }
 .container {
   background: white;
   border-radius: 13px;
+  display: flex;
 }
+.formContainer {
+  padding: 28px;
+}
+.img {
+  margin-top: 158px;
+  padding: 39px;
+}
+
 h1 {
   padding: 20px 0;
   color: #483698;
@@ -112,8 +122,28 @@ h2 {
   padding-bottom: 10px;
   font-size: 27px;
 }
-b-button {
-  color: orange;
+.Statecity {
+  display: flex;
+  justify-content: space-between;
+  width: 500px;
+}
+#example-input-1 {
+  color: aquamarine;
+  width: 240px;
+}
+
+#example-input-2 {
+  width: 240px;
+}
+
+button {
+  background: #483698;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  align-items: center;
+  height: 35px;
+  width: 500px;
 }
 </style>
 <script>
@@ -209,20 +239,6 @@ export default {
       } else {
         alert(JSON.stringify(this.form));
       }
-    },
-    onReset(event) {
-      event.preventDefault();
-      // Reset our form values
-      this.form.name = "";
-      this.form.cpf = "";
-      this.form.number = "";
-      this.form.state = null;
-      this.form.city = null;
-      // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
     },
   },
 };
